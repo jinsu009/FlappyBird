79a064a877fa0692c27ff2d72eb07d7efd7c8215
jinsu <dnqlthsus9@gmail.com>
2024-05-11 19:02:15 +0900

    240511_1강_화면에 이미지 배치

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,80 +9,42 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+   
+    // 초기화
     override func didMove(to view: SKView) {
+        let land = SKSpriteNode(imageNamed: "land")
+        land.position = CGPoint(x: self.size.width/2, y: 50)
+        // 화면에 겹치는 정도..?
+        land.zPosition = 3
+        self.addChild(land)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let sky = SKSpriteNode(imageNamed: "sky")
+        sky.position = CGPoint(x: self.size.width/2, y: 100)
+        sky.zPosition = 1
+        self.addChild(sky)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let pipeUp = SKSpriteNode(imageNamed: "pipe")
+        pipeUp.position = CGPoint(x: self.size.width/2, y: 100)
+        pipeUp.zPosition = 2
+        self.addChild(pipeUp)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let pipeDown = SKSpriteNode(imageNamed: "pipe")
+        pipeDown.position = CGPoint(x: self.size.width/2, y: self.size.height)
+        pipeDown.zPosition = 2
+        pipeDown.xScale = -1 // x방향으로 180' 회전
+        pipeDown.zRotation = .pi // 이미지가 180' 회전 , 위 아래가 뒤집힘
+        self.addChild(pipeDown)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let ceiling = SKSpriteNode(imageNamed: "ceiling")
+        ceiling.position = CGPoint(x: self.size.width/2, y: 300)
+        ceiling.zPosition = 3
+        self.addChild(ceiling)
+        
+        let bird = SKSpriteNode(imageNamed: "bird")
+        bird.position = CGPoint(x: self.size.width/2, y: 200)
+        bird.zPosition = 2
+        self.addChild(bird)
+        
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+   
 }
